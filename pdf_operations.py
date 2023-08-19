2a4a7cce47c6159cb688f7c10bde88375b2c9536
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2023-08-19 23:17:15 +0200

    feat: implemented pdf page extraction, not complete

--- pdf_operations.py
+++ pdf_operations.py
@@ -9,7 +9,7 @@ class BasicPDFOperations:
         pass
 
     @staticmethod
-    def text_extractor(self, input_path, pages, output_path):
+    def text_extractor(input_path, pages, output_path):
         try:
             # Call the read_pdf method to initialize self.pdf_reader
             # self.read_pdf(input_path)
@@ -35,7 +35,30 @@ class BasicPDFOperations:
         except Exception as e:
             raise Exception(f"An error occurred while extracting text: {str(e)}")
 
+    @staticmethod
+    def extract_pages(input_path, pages, output_path):
+        try:
+            with open(input_path, 'rb') as pdf_file:
+                pdf_reader = PyPDF2.PdfReader(pdf_file)
 
+                # Check if all page numbers are within the valid range
+                invalid_pages = [page_num for page_num in pages if not (1 <= page_num <= len(pdf_reader.pages))]
+                if invalid_pages:
+                    raise ValueError(f"Invalid page numbers: {invalid_pages}")
+
+                # Create a new PDF writer
+                pdf_writer = PyPDF2.PdfWriter()
+
+                # Iterate through the specified pages
+                for page_num in pages:
+                    page = pdf_reader.pages[page_num - 1]  # Adjust for 0-based indexing
+                    pdf_writer.add_page(page)
+
+                # Write the selected pages to the output PDF file
+                with open(output_path, 'wb') as output_file:
+                    pdf_writer.write(output_file)
+        except Exception as e:
+            raise Exception(f"An error occurred while extracting pages: {str(e)}")
 
     # TODO: Extracting a single or multiple pages and saving to a new pdf
 
